34a2e8a6575ebf4f18f8c5f8a5dc9ad6c15e0d21
ycr5007 <86907186+ycr5007@users.noreply.github.com>
2022-03-04 14:45:32 +0900

    JAVA 3일차(2)_Scanner

---
+++ ch2/src/condition/ScannerEx1.java
@@ -0,0 +1,18 @@
+package condition;
+
+import java.util.Scanner;
+
+public class ScannerEx1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("첫번째 수 입력 >> ");
+		int num1 = sc.nextInt();
+		System.out.print("두번째 수 입력 >> ");
+		int num2 = sc.nextInt();
+
+		System.out.println("입력한 숫자의 덧셈 결과는 " + (num1 + num2));
+		
+		sc.close();
+	}
+}
