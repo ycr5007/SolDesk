29cd204084c8f3c672a4283eea29a3ef72eb4d0c
ycr5007 <86907186+ycr5007@users.noreply.github.com>
2022-03-14 10:02:44 +0900

    JAVA 8일차(1)_Class Diagram (Amateras)

---
+++ ch3/src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+	
+	// exam - class diagram 통해서 자동생성한 클래스
+	
+    private String name;
+    private String kind;
+
+    public void method1(){
+    }
+
+    public int method2(){
+        return 0;
+    }
+
+}
