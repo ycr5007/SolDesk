af4d7b0fb1bd8c2547b4a5af9cb2aac613ec403b
yangchanryeol <ycr5007@gmail.com>
2022-05-20 17:58:24 +0900

    리스트

---
+++ PythonSource/basic/list/list1.py
@@ -0,0 +1,17 @@
+# list 자료형 ( 배열과 유사한 개념이나, 배열 X )
+# 다양한 형태의 자료들을 담을 수 있다. ( 자료형 혼합 X )
+
+# 생성
+list1 = []
+list2 = ['a', 'b', 'c']
+list3 = ['a', 'b', 'c', 1, 2]
+list4 = [1, 2, 3, 4, 5, 6.5]
+list5 = [1, 2, ['Life', 'is', 'short']]
+list6 = list()
+
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+print(list5)
+print(list6)
